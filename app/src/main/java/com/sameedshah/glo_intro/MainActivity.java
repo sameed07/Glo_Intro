fe05e8b48f3de5631a461497864215b7d06704ee
Sameed Shah <sameedshah9@gmail.com>
2019-04-17 18:48:30 +0500

    adapter added

--- app/src/main/java/com/sameedshah/glo_intro/MainActivity.java
+++ app/src/main/java/com/sameedshah/glo_intro/MainActivity.java
@@ -1,13 +1,36 @@
 package com.sameedshah.glo_intro;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ViewPager viewPager;
+    SliderAdapter sliderAdapter;
+    LinearLayout linearLayout;
+    TextView[] mDots;
+    Button btnNext,btnBack;
+    int currentPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.mViewPager);
+        linearLayout = findViewById(R.id.dotLayout);
+
+        btnNext = findViewById(R.id.btnNext);
+        btnBack = findViewById(R.id.btnBack);
+
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+       // addDotsIndecator(0);
+
     }
 }
